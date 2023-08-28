790eff7c15eb1b730ecc1e684bd48af605505f21
BohoBabe <slenorovitz@gmail.com>
2023-08-28 09:46:00 -0700

    added index.js route handlers

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.json());
 
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server for ${activity} running on port ${PORT}!`);
+    });
+});
+
 mongoose.connect('mongodb://localhost:27017/socialmedia', {
     useNewUrlParser: true,
     uneUnifiedTopology: true,
